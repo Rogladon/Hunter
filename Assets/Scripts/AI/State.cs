bd87b103f58aacf2084e8f507d8e926e599a5830
Rogladon <54538199+Rogladon@users.noreply.github.com>
2021-03-19 16:23:05 +0300

    Update State.cs

--- Assets/Scripts/AI/State.cs
+++ Assets/Scripts/AI/State.cs
@@ -13,17 +13,19 @@ namespace Hunter {
 		public List<State> nextStates { get; protected set; }
 		public State currentState { get; protected set; }
 		public int priority { get; protected set; }
-
+		//Инициализация состояния
 		abstract public void Init();
+		//Условие перехода на это состояние
 		abstract public bool Condition();
-
+		//Основное действие (выполняется как Update()
 		abstract public void Behaviour();
+		//Выполняется при переходе на это состояние один раз
 		virtual public void StartState() {
 		}
-
+		//При окончании состония (когда новое начинается)
 		virtual public void EndState() {
 		}
-
+		//А это перед инициализацией стартовой, тут пишется собственно в какие состояния можно перейти из этого и приоритет состояния
 		protected void PrevInit() {
 			nextStates = new List<State>();
 			states = new List<State>();
